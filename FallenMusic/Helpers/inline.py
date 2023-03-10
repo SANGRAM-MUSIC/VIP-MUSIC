be222387882572de06ba13d0d6f854bb06a50531
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-03-10 11:59:02 +0530

    Update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -1,25 +1,3 @@
-# MIT License
-#
-# Copyright (c) 2023 AnonymousX1025
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 import config
@@ -49,7 +27,7 @@ buttons = InlineKeyboardMarkup(
       InlineKeyboardButton(text="🍒𝐂𝐨𝐦𝐞 𝐁𝐚𝐛𝐲😘", url=f"https://t.me/TG_FRIENDSS")
     ],
     [
-      InlineKeyboardButton(text="★ 𝐎𝐰𝐧𝐞𝐫'𝐱𝐃 ★", url=f"https://t.me/THE_VIP_BOY"),
+      InlineKeyboardButton(text="★ 𝐎𝐰𝐧𝐞𝐫 ★", url=f"https://t.me/THE_VIP_BOY"),
       
   ],
 ]
@@ -70,7 +48,7 @@ pm_buttons = [
         InlineKeyboardButton(
             text="🌱sᴏᴜʀᴄᴇ🌱", url="https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
         ),
-        InlineKeyboardButton(text="★ 𝐎𝐰𝐧𝐞𝐫'𝐱𝐃 ★", user_id=config.OWNER_ID),
+        InlineKeyboardButton(text="★ 𝐎𝐰𝐧𝐞𝐫 ★", user_id=config.OWNER_ID),
     ],
 ]
 
@@ -90,7 +68,7 @@ gp_buttons = [
         InlineKeyboardButton(
             text="🌱sᴏᴜʀᴄᴇ🌱", url="https://github.com/THE-VIP-BOY-OP/VIP-MUSIC"
         ),
-        InlineKeyboardButton(text="★ 𝐎𝐰𝐧𝐞𝐫'𝐱𝐃 ★", user_id=config.OWNER_ID),
+        InlineKeyboardButton(text="★ 𝐎𝐰𝐧𝐞𝐫 ★", user_id=config.OWNER_ID),
     ],
 ]
 
